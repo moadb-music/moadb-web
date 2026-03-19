c80193b0421d4fb86c7be99a9d441f9ca082a673
guidon <crawzlergg@gmail.com>
2026-03-19 17:21:40 -0300

    payment-methods-perfect-outfix

--- fixcss3.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const file = 'c:/Users/gui-2/Desktop/DistroKid/Site/moadb-site/src/App.css';
-const lines = fs.readFileSync(file, 'utf8').split('\n');
-
-// line 664 (index 663) is orphaned "  width:100%;" — remove it, it's already inside .news-empty
-lines.splice(663, 1);
-
-fs.writeFileSync(file, lines.join('\n'), 'utf8');
-console.log('Done. Total lines:', lines.length);
